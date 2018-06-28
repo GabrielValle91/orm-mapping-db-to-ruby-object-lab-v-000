e4175a5b2c79e8b24dcea4fbd33faea0fb23229f
Gabriel Valle <gvalle91@gmail.com>
2018-06-28 16:49:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,7 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
     sql = <<-SQL
-      SELECT name FROM students
+      SELECT * FROM students
       WHERE name = ?
       LIMIT 1
     SQL
