725f4c0778fad28569d23581ddcda46d1189b5d3
Gabriel Valle <gvalle91@gmail.com>
2018-06-28 16:46:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,6 +22,38 @@ class Student
     end
   end
 
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL
+      SELECT * FROM students
+      LIMIT ?
+    SQL
+    DB[:conn].execute(sql,number)
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql)
+  end
+
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
